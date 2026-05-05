c08f9d78b2cb5c43b9905d7f61eac78c8d25b1c7
Raphael Rodriguez <rapcole12@vt.edu>
2026-05-05 10:15:20 -0400

    Adding Data for Training

--- covert_pred_to_bits.m
+++ covert_pred_to_bits.m
@@ -1,8 +1,8 @@
-for k = 1:3
+for k = 0:3
     % -----------------------
     % Load Case_k.mat
     % -----------------------
-caseFile = sprintf('/home/rapcole12/Documents/RFLLM/dataset/simple/output/Case_%d.mat', k);
+caseFile = sprintf('/home/rapcole12/Documents/RFLLM/results/prediction_batch/pred_test_000%d.mat', k);
     file = load(caseFile);
 
     % -----------------------
@@ -11,7 +11,7 @@ caseFile = sprintf('/home/rapcole12/Documents/RFLLM/dataset/simple/output/Case_%
     iq = file.iq;
     iq = single(iq(:));   % ensure column vector, float32
 
-    predIQ = sprintf('/home/rapcole12/Documents/RFLLM/dataset/simple/pred%d.bin', k);
+    predIQ = sprintf('/home/rapcole12/Documents/RFLLM/dataset/simple/pred_000%d.bin', k);
     fid = fopen(predIQ, 'wb');
     fwrite(fid, [real(iq) imag(iq)].', 'float32');  % interleaved I Q
     fclose(fid);
@@ -22,7 +22,7 @@ caseFile = sprintf('/home/rapcole12/Documents/RFLLM/dataset/simple/output/Case_%
     bits = file.bits(:);        % ensure column vector
     bits = uint8(bits ~= 0);    % force to 0/1 uint8
 
-    predBits = sprintf('/home/rapcole12/Documents/RFLLM/dataset/simple/pred_bits%d.bin', k);
+    predBits = sprintf('/home/rapcole12/Documents/RFLLM/dataset/simple/pred_bits_000%d.bin', k);
     fid = fopen(predBits, 'wb');
     fwrite(fid, bits, 'uint8');
     fclose(fid);
